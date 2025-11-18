c064bc5b529475303eefdc3b5b10a14b196b2675
parmarkrishnaa <parmarkrishna050611@gmail.com>
2025-11-18 12:00:00 +0530

    counter

--- day-4-counter/src/components/Counter.jsx
+++ day-4-counter/src/components/Counter.jsx
@@ -1,44 +1,64 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import "./Counter.css";
 
 const Counter = () => {
-    const [count, setCount] = useState(0);
-    const [isActive, setIsActive] = useState(false);
+  const [count, setCount] = useState(0);
+  const [isActive, setIsActive] = useState(false);
 
-    const handleStart = () => {
-      setIsActive(true);
+  const handleStart = () => {
+    setIsActive(true);
+  };
+
+  const handleStop = () => {
+    setIsActive(false);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+    setIsActive(false);
+  };
+
+  useEffect(() => {
+    let interval;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setCount((count) => count + 1);
+      }, 1000);
     }
 
-    const handleStop = () => {
-      setIsActive(false);
-    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isActive]);
 
-    const handleReset = () => {
-      setCount(0);
-      setIsActive(false);
-    }
+  const format = (totalSeconds) => {
+    const hrs = String(Math.floor(totalSeconds / 3600)).padStart(2, "0");
+    const min = String(Math.floor((totalSeconds % 3600) / 60)).padStart(2, "0");
+    const sec = String(totalSeconds % 60).padStart(2, "0");
 
-    useEffect(() => {
-      let interval;
-
-      if(isActive){
-        interval = setInterval(() => {
-          setCount((count) => count + 1);
-        }, 1000);
-      }
-
-      return(() => {
-        clearInterval(interval);
-      })
-    }, [isActive]);
+    return `${hrs}:${min}:${sec}`;
+  };
 
   return (
-    <div>
-      <h2>Count {count}</h2>
-      <button onClick={handleStart}>Start</button>
-      <button onClick={handleStop}>Stop</button>
-      <button onClick={handleReset}>Reset</button>
-    </div>
-  )
-}
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="stopwatch">
+            <div className="timer-wrapper">
+              <div className="timer">
+                <h2>{format(count)}</h2>
+              </div>
+            </div>
 
-export default Counter
+            <button onClick={handleStart}>Start</button>
+            <button onClick={handleStop}>Stop</button>
+            <button onClick={handleReset}>Reset</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Counter;
